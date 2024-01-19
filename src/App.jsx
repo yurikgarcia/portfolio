7a00747a34703acb227832dc43bd07d7bc3dee9d
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2024-01-20 02:45:17 +0300

    change to green bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import "./App.css";
-import FirstView from ".//components/FirstView"
-import SecondView from "./components/SecondView"
+import FirstView from ".//components/FirstView";
+import SecondView from "./components/SecondView";
 
 function App() {
-
-
-
   return (
     <>
       {/* First view with shiny gradient background */}
       <FirstView />
 
-
       {/* Second view with shiny white background */}
       <SecondView />
-
     </>
   );
 }
