488d68f08791e69e0bdc235157490e74f3ec74b2
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2024-01-21 01:33:32 +0300

    infinite styling

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -26,29 +26,28 @@ function FirstView() {
               }}
             />
           </div>
-          {/* <div className="profile-name">
-            <h1 style={{ fontSize: "6vw", margin: "0" }}>Yurik Garcia</h1>
-          </div> */}
-          <img
-            src={YURIK}
-            alt="Yurik Garcia"
-            style={{
-              width: "80%",
-              maxWidth: "200px",
-              height: "auto",
-              marginLeft: "-20px",
-            }}
-          />
-          <img
-            src={GARCIA}
-            alt="Yurik Garcia"
-            style={{
-              width: "80%",
-              maxWidth: "200px",
-              height: "auto",
-              marginLeft: "-10px",
-            }}
-          />
+          <div className="profile-names">
+            <img
+              src={YURIK}
+              alt="Yurik Garcia"
+              style={{
+                width: "50%", // Adjust the percentage as needed
+                maxWidth: "200px", // Set a maximum width
+                height: "auto",
+                marginLeft: "-20px",
+              }}
+            />
+            <img
+              src={GARCIA}
+              alt="Yurik Garcia"
+              style={{
+                width: "50%", // Adjust the percentage as needed
+                maxWidth: "200px", // Set a maximum width
+                height: "auto",
+                marginLeft: "-10px",
+              }}
+            />
+          </div>
         </div>
         <div className="profile-description">
           <p style={{ fontSize: "2.5vw", margin: "0" }}>
@@ -58,7 +57,6 @@ function FirstView() {
           </p>
         </div>
       </div>
-      {/* <Burger /> */}
     </div>
   );
 }
