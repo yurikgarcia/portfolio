aceea8f3284baef9bbf9f186c0d6d2ef8c09a6a2
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2024-01-20 22:31:40 +0300

    added font and styling

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import "../App.css";
 import Burger from "./Burger";
 import NavBar from "./NavBar";
+import ProfileAvatar from "../assets/ProfileAvatar.gif";
 
 function FirstView() {
   return (
-    <div className="app-container ">
+    <div className="app-container center-text">
       <div className="navbar">
         <NavBar />
       </div>
-      <h1>Yurik Garcia</h1>
+      <div className="profile-container">
+        <div className="profile-info">
+          <div className="profile-avatar">
+            <img src={ProfileAvatar} alt="Profile Avatar" style={{ width: "100px", height: "100px", borderRadius: "50%" }} />
+          </div>
+          <div className="profile-name">
+            <h1>Yurik Garcia</h1>
+          </div>
+        </div>
+        <div className="profile-description">
+          <p>
+            Full Stack Software Engineer blending precision in code with a
+            passion for innovation, <br /> crafting elegant solutions, and fostering
+            a collaborative culture in every project.
+          </p>
+        </div>
+      </div>
       {/* <Burger /> */}
     </div>
   );
