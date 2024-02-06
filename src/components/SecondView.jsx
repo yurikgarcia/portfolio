62e2d4f122b3cba26c8d000a46f80aeabbe2b980
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2024-02-06 07:29:28 +0300

    working on styling :::sigh:::

--- src/components/SecondView.jsx
+++ src/components/SecondView.jsx
@@ -12,122 +12,140 @@ function SecondView() {
     <div
       className="second-view"
       style={{
-        display: "flex",
         justifyContent: "center",
         height: "100vh",
-        flexDirection: "column",
       }}
     >
       <div
-        className="content-container"
-        style={{ width: "80%", display: "flex",}}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
       >
-        <div className="shelf_double" style={{ flex: 1 }}>
-          <img
-            src={shelf_double}
-            alt="Yurik Garcia"
-            style={{
-              width: "100%",
-              maxWidth: "250vh",
-            }}
-          />
-        </div>
-        <div style={{ flex: 1, color: "black" }}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: "4.5vh",
-            }}
-          >
+        <div
+          className="content-container"
+          style={{ width: "95%", display: "flex", flexDirection: "row", marginTop: "10vh"}}
+        >
+          <div className="shelf_double" style={{ flex: 1 }}>
             <img
-              src={shelf_logo}
-              alt="Shelf Logo"
+              src={shelf_double}
+              alt="Yurik Garcia"
               style={{
-                maxWidth: "50px",
+                width: "100%",
+                maxWidth: "250vh",
               }}
             />
-            <h1 className="caprasimo">Shelf</h1>
           </div>
-          <p className="app-description">
-            An inventory management application that effortlessly manages your
-            assets digitally, whether you’re in the warehouse or on the go!
-          </p>
-          <p className="app-roles">  FULL-STACK SOFTWARE DEVELOPER // UX & UI DESIGN // PRODUCT MANAGER </p>
+          <div style={{ flex: 1, color: "black" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: "4.5vh",
+              }}
+            >
+              <img
+                src={shelf_logo}
+                alt="Shelf Logo"
+                style={{
+                  maxWidth: "50px",
+                }}
+              />
+              <h1 className="caprasimo">Shelf</h1>
+            </div>
+            <div style={{width:'75%'}}>
+            <p className="app-description">
+              An inventory management application that effortlessly manages your
+              assets digitally, whether you’re in the warehouse or on the go!
+            </p>
+            <p className="app-roles">
+              {" "}
+              FULL-STACK SOFTWARE DEVELOPER // UX & UI DESIGN // PRODUCT MANAGER{" "}
+            </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div
-        className="content-container"
-        style={{ width: "80%", display: "flex", marginTop: "5vh"}}
-      >
-        <div style={{ flex: 1, color: "black", marginLeft: '5vw' }}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: "4.5vh",
-            }}
-          >
+        <div
+          className="content-container"
+          style={{ width: "95%", display: "flex", flexDirection: "row",marginTop: "5vh"}}
+        >
+          <div style={{ flex: 1, color: "black", marginLeft: "5vw" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: "4.5vh",
+              }}
+            >
+              <img
+                src={ezLogo}
+                alt="ez pass logo"
+                style={{
+                  maxWidth: "50px",
+                }}
+              />
+              <h1 className="caprasimo">EzPass</h1>
+            </div>
+            <div style={{width:'75%'}}>
+            <p className="app-description">
+              An inventory management application that effortlessly manages your
+              assets digitally, whether you’re in the warehouse or on the go!
+            </p>
+            <p className="app-roles">
+              {" "}
+              FULL-STACK SOFTWARE DEVELOPER // UX & UI DESIGN // PRODUCT MANAGER{" "}
+            </p>
+            </div>
+          </div>
+          <div className="ez_double" style={{ flex: 1 }}>
             <img
-              src={ezLogo}
+              src={ezDouble}
               alt="ez pass logo"
               style={{
-                maxWidth: "50px",
+                width: "100%",
+                maxWidth: "250vh",
               }}
             />
-            <h1 className="caprasimo">EzPass</h1>
           </div>
-          <p className="app-description">
-            An inventory management application that effortlessly manages your
-            assets digitally, whether you’re in the warehouse or on the go!
-          </p>
-          <p className="app-roles">  FULL-STACK SOFTWARE DEVELOPER // UX & UI DESIGN // PRODUCT MANAGER </p>
         </div>
-        <div className="ez_double" style={{ flex: 1 }}>
-          <img
-            src={ezDouble}
-            alt="ez pass logo"
-            style={{
-              width: "100%",
-              maxWidth: "250vh",
-            }}
-          />
-        </div>
-      </div>
-      <div
-        className="content-container"
-        style={{ width: "80%", display: "flex",}}
-      >
-        <div className="bene_double" style={{ flex: 1 }}>
-          <img
-            src={beneDouble}
-            alt="bene double"
-            style={{
-              width: "100%",
-              maxWidth: "250vh",
-            }}
-          />
-        </div>
-        <div style={{ flex: 1, color: "black" }}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: "4.5vh",
-            }}
-          >
-      
-            <h1 className="companyName1">bene</h1>
-            <h1 className="companyName2">Nav</h1>
+        <div
+          className="content-container"
+          style={{ width: "95%", display: "flex", flexDirection: "row",marginTop: "5vh"}}
+        >
+          <div className="bene_double" style={{ flex: 1 }}>
+            <img
+              src={beneDouble}
+              alt="bene double"
+              style={{
+                width: "100%",
+                maxWidth: "250vh",
+              }}
+            />
           </div>
-          <p className="app-description">
-            An inventory management application that effortlessly manages your
-            assets digitally, whether you’re in the warehouse or on the go!
-          </p>
-          <p className="app-roles">  FULL-STACK SOFTWARE DEVELOPER // UX & UI DESIGN // PRODUCT MANAGER </p>
+          <div style={{ flex: 1, color: "black" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: "4.5vh",
+              }}
+            >
+              <h1 className="companyName1">bene</h1>
+              <h1 className="companyName2">Nav</h1>
+            </div>
+            <div style={{width:'75%'}}>
+            <p className="app-description">
+              A mobile application designed to be a cradle to grave app that
+              allows end users the ability to shop, merge and track health
+              benefits!
+            </p>
+            <p className="app-roles"> FRONT-END SOFTWARE DEVELOPER</p>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
